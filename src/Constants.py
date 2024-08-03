3d2667e103408da8f06df0b26e10836ebdf3f80e
TTeuZ <pmla.luza@gmail.com>
2024-08-03 01:22:10 -0300

    [@TTeuZ | @victorstella] WIP begin

--- src/Constants.py
+++ src/Constants.py
@@ -1,6 +1,7 @@
 # Constants
 DELIMITER = "~"
 BUFFER_SIZE = 1024
+TIMEOUT = 10
 
 # Enums - Types
 CONNECTION = -1
--- src/Node.py
+++ src/Node.py
@@ -1,32 +1,36 @@
 import sys, os; sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
 
-from Constants import BUFFER_SIZE, CONNECTION
+from Constants import BUFFER_SIZE, CONNECTION, TIMEOUT
 from Package import Package
 import select
 import socket
 
 class Node:
     def __init__(self, port, neighbor, neighbor_port, token):
-        self.port = port
-        self.neighbor = neighbor
-        self.neighbor_port = neighbor_port
-        self.token = bool(token)
-
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        self.neighbor_ip = self.ip if neighbor == "local" else socket.gethostbyname(neighbor)
+        self.neighbor, self.neighbor_port = neighbor, neighbor_port
+
         self.hotsname = socket.gethostname()
         self.ip = socket.gethostbyname(self.hotsname)
+        self.port = port
 
-        if neighbor == "local":
-            self.neighbor_ip = self.ip
-        else:
-            self.neighbor_ip = socket.gethostbyname(self.neighbor)
-        
+        self.token = bool(token)
         self.socket.bind((self.ip, self.port))
+
+
+    def send_package(self, package):
+        while True:
+            ready = select.select([], [self.socket], [], TIMEOUT)[1]
+            if ready:
+                self.socket.sendto(package.get_message().encode(), (self.neighbor_ip, self.neighbor_port))
+                return
 
 
     def recv_package(self):
         while True:
-            ready = select.select([self.socket], [], [], 10)[0]
+            ready = select.select([self.socket], [], [], TIMEOUT)[0]
             if ready:
                 data, _ = self.socket.recvfrom(BUFFER_SIZE)
                 return Package(data.decode())
@@ -36,7 +40,7 @@ class Node:
         print("Establishing connection...")
         if self.token:
             connection_package = Package(src=self.ip, dst=None, token=False, type=CONNECTION, data="-1")
-            self.socket.sendto(connection_package.get_message().encode(), (self.neighbor_ip, self.neighbor_port))
+            self.send_package(connection_package)
             
             package = self.recv_package()
             print(package.data)
@@ -45,10 +49,9 @@ class Node:
 
             if package.type == CONNECTION:
                 splited_data = package.data.split("-")
-                count = splited_data[-1]
-                count += 1
-
+                count = splited_data[-1] + 1
                 data = splited_data[0] + f"({self.hostname}, {self.ip})" + f"-{count}"
+
                 connection_package = Package(src=self.ip, dst=None, token=False, type=CONNECTION, data=data)
-                self.socket.sendto(connection_package.get_message().encode(), (self.neighbor_ip, self.neighbor_port))
+                self.send_package(connection_package)
 
